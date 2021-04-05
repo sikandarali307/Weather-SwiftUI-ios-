4d02b89c13e06c41acad9f9f65485ae1031099d3
sikandar <sikandar.ali307@gmail.com>
2021-04-05 10:45:14 +0500

    First commit Message

--- SwiftUIDemo/weatherButton.swift
+++ SwiftUIDemo/weatherButton.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import SwiftUI
+struct weatherButton: View {
+    var title:String
+    var textcolor:Color
+    var bgcolor:Color
+    var body: some View {
+        Text(title)
+                .frame(width: 280, height: 50)
+                .font(.system(size: 20, weight: .bold, design: .default))
+                .foregroundColor(textcolor)
+                .background(bgcolor)
+                .cornerRadius(12)
+        }
+    
+}
