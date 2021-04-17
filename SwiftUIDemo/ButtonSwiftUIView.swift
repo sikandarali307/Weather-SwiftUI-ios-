549ecf28934ced7f986ea55d7dece0fd48bdc1b5
sikandar <sikandar.ali307@gmail.com>
2021-04-17 21:17:00 +0500

    new class added for just checking is this working or not

---
+++ SwiftUIDemo/ButtonSwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonSwiftUIView.swift
+//  SwiftUIDemo
+//
+//  Created by Sikandar Ali on 17/04/2021.
+//
+
+import SwiftUI
+
+struct ButtonSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonSwiftUIView()
+    }
+}
